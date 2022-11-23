6b39d86c0cf3b1a2c0fe6eb6d6fc458b9308e215
MrWaradana <muhammadridhowaradana@gmail.com>
2022-11-23 18:24:15 +0700

    change padding IG btn

--- app/coming-soon/page.tsx
+++ app/coming-soon/page.tsx
@@ -9,7 +9,7 @@ export default function ComingSoon() {
                 <h1 className="text-5xl md:text-5xl lg:text-7xl text-center bg-gradient-to-r text-transparent bg-clip-text from-[#07003F] via-[#3D4BE0] to-[#D0D4F8] font-bold">
                     COMING SOON
                 </h1>
-                <a href="https://www.instagram.com/petrolida.2023/" className="absolute bottom-36 flex bg-[#07003F] rounded-full px-5 py-4 transition duration-300 hover:-translate-y-2" target="_blank">
+                <a href="https://www.instagram.com/petrolida.2023/" className="absolute bottom-36 flex bg-[#07003F] rounded-full px-12 py-4 transition duration-300 hover:-translate-y-2" target="_blank">
                     <span className="pr-3">
                         <img src="/coming-soon/bxl_instagram-alt.svg" alt="instagram" className="w-full" />
                     </span>
