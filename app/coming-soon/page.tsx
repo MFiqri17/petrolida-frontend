98f59e0932918cbd09d2170fe3ef36b9e24d0598
MrWaradana <muhammadridhowaradana@gmail.com>
2022-11-23 18:18:39 +0700

    feat: coming-soon page

---
+++ app/coming-soon/page.tsx
@@ -0,0 +1,23 @@
+export default function ComingSoon() {
+    return (
+        <div>
+            <main className="bg-[#EDEEF3] bg-[center_18rem] h-screen overflow-hidden flex flex-col justify-center items-center" style={{
+                backgroundImage: "url('/coming-soon/triangle.png')",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "contain",
+            }}>
+                <h1 className="text-5xl md:text-5xl lg:text-7xl text-center bg-gradient-to-r text-transparent bg-clip-text from-[#07003F] via-[#3D4BE0] to-[#D0D4F8] font-bold">
+                    COMING SOON
+                </h1>
+                <a href="https://www.instagram.com/petrolida.2023/" className="absolute bottom-36 flex bg-[#07003F] rounded-full px-5 py-4 transition duration-300 hover:-translate-y-2" target="_blank">
+                    <span className="pr-3">
+                        <img src="/coming-soon/bxl_instagram-alt.svg" alt="instagram" className="w-full" />
+                    </span>
+                    <span className="text-[#EDEEF3]">
+                        Our Instagram
+                    </span>
+                </a>
+            </main>
+        </div>
+    );
+}
