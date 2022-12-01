5bb6ab60fe66c67d128766b6a149a4d90bdc8b38
Ramadhana R <ddhanarr@gmail.com>
2022-12-01 22:03:43 +0700

    fix: folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import './globals.css'
+import Navbar from '../components/navbar'
+import Footer from './footer'
 
 export default function RootLayout({
   children,
@@ -12,7 +14,11 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
---
+++ data/footerList.ts
@@ -0,0 +1,78 @@
+const footerListData = [
+  {
+    id: 1,
+    title: 'Product',
+    content: [
+      {
+        lable: 'About us',
+        href: '',
+      },
+      {
+        lable: 'Event',
+        href: '',
+      },
+      {
+        lable: 'Competitions',
+        href: '',
+      },
+      {
+        lable: 'Non Competitions',
+        href: '',
+      },
+      {
+        lable: 'FAQ',
+        href: '',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Competitions',
+    content: [
+      {
+        lable: 'Oil Rig Desain',
+        href: '',
+      },
+      {
+        lable: 'Fracturing Fluid Desain',
+        href: '',
+      },
+      {
+        lable: 'Petrosmart',
+        href: '',
+      },
+      {
+        lable: 'Paper',
+        href: '',
+      },
+      {
+        lable: 'Business Case',
+        href: '',
+      },
+      {
+        lable: 'Case Study',
+        href: '',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Non Competitions',
+    content: [
+      {
+        lable: 'CV 101',
+        href: '',
+      },
+      {
+        lable: 'Interview 101',
+        href: '',
+      },
+      {
+        lable: 'Career Paths',
+        href: '',
+      },
+    ],
+  },
+]
+
+export default footerListData
