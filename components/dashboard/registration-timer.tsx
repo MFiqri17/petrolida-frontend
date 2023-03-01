6c8a515b57e146d3259d8ffe21d57a54dd84733c
Ramadhana R <ddhanarr@gmail.com>
2023-03-01 08:44:54 +0700

    feat: dashboard registration timer revision

--- components/dashboard/registration-timer.tsx
+++ components/dashboard/registration-timer.tsx
@@ -8,28 +8,15 @@ export default function RegistrationTimer({
   endRegistration: string
 }) {
   const endRegistrationDate = new Date(endRegistration)
-  const { days, hours, minutes } = useTimer({
+  const { days } = useTimer({
     expiryTimestamp: endRegistrationDate,
     onExpire: () => console.log('onExpire called'),
   })
   return (
-    <div className="absolute left-0 right-0 bottom-4 mx-6 flex justify-between rounded-full bg-whiteb px-3 py-2 font-semibold">
-      {new Date() > new Date(startRegistration) ? (
+    <div className="absolute left-0 right-0 bottom-4 mx-6 flex flex-col items-center justify-center gap-x-2 rounded-full bg-whiteb py-2 font-semibold sm:flex-row">
+      {new Date() > new Date(endRegistration) ? (
         <>
-          <p>Register Ends</p>
-          <div>
-            <span className="rounded-md bg-primary p-1 text-whiteb">
-              {days}
-            </span>{' '}
-            :
-            <span className="rounded-md bg-primary p-1 text-whiteb">
-              {hours}
-            </span>{' '}
-            :
-            <span className="rounded-md bg-primary p-1 text-whiteb">
-              {minutes}
-            </span>
-          </div>
+          <p>Registration Closed</p>
         </>
       ) : (
         <>
@@ -37,16 +24,9 @@ export default function RegistrationTimer({
           <div>
             <span className="rounded-md bg-primary p-1 text-whiteb">
               {days}
-            </span>{' '}
-            :
-            <span className="rounded-md bg-primary p-1 text-whiteb">
-              {hours}
-            </span>{' '}
-            :
-            <span className="rounded-md bg-primary p-1 text-whiteb">
-              {minutes}
             </span>
           </div>
+          <p>Days Again</p>
         </>
       )}
     </div>
